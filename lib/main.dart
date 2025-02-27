2c0cb8614cd916f03d4d9a57c00a483e3806aee7
honorine22 <higiranez@gmail.com>
2025-02-27 14:54:27 +0200

    Deliver welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,41 @@
+// main.dart
 import 'package:flutter/material.dart';
+import 'package:link_life/screens/onboarding/welcome_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const LinkLifeApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class LinkLifeApp extends StatelessWidget {
+  const LinkLifeApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'LinkLife Rwanda',
+      theme: ThemeData(primarySwatch: Colors.red),
+      initialRoute: '/welcome',
+      routes: {
+        '/welcome': (context) => WelcomeScreen(),
+        '/intro': (context) => IntroScreen(),
+        '/signup': (context) => SignUpScreen(),
+        '/login': (context) => LoginScreen(),
+        '/dashboard': (context) => DashboardScreen(),
+        '/eventDiscovery': (context) => EventDiscoveryScreen(),
+        '/hospitalDirectory': (context) => HospitalDirectoryScreen(),
+        '/appointmentBooking': (context) => AppointmentBookingScreen(),
+        '/qrDisplay': (context) => QRDisplayScreen(),
+        '/qrScanner': (context) => QRScannerScreen(),
+        '/attendanceTracking': (context) => AttendanceTrackingScreen(),
+        '/recoveryTips': (context) => RecoveryTipsScreen(),
+        '/eligibilityReminder': (context) => EligibilityReminderScreen(),
+        '/notifications': (context) => NotificationsScreen(),
+        '/editProfile': (context) => EditProfileScreen(),
+        '/notificationSettings': (context) => NotificationSettingsScreen(),
+        '/privacySettings': (context) => PrivacySettingsScreen(),
+        '/eventManagement': (context) => EventManagementScreen(),
+        '/volunteerTracking': (context) => VolunteerTrackingScreen(),
+      },
     );
   }
 }
--- lib/screens/onboarding/welcome_screen.dart
+++ lib/screens/onboarding/welcome_screen.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to LinkLife Rwanda',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text('Revolutionizing blood donation with AI and Blockchain'),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/intro'),
+              child: Text('Get Started'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
